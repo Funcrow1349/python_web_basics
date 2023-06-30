6b7201fe60b665543485f2fe1e850c12f11ca66f
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-06-30 22:08:51 +0300

    Petstagram Workshop

--- petstagram/petstagram/pets/admin.py
+++ petstagram/petstagram/pets/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from petstagram.pets.models import Pet
+
+
+class PetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
 # Register your models here.
+admin.site.register(Pet)
