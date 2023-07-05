8e88409cd972a0a2bd2675f0d4eac3343858dc73
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-07-05 11:17:31 +0300

    Petstagram Workshop

--- petstagram/petstagram/common/views.py
+++ petstagram/petstagram/common/views.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render, redirect, resolve_url
 from pyperclip import copy
 
+from petstagram.common.forms import CommentForm, SearchForm
 from petstagram.common.models import Like
 from petstagram.photos.models import Photo
 
 
 def index(request):
     all_photos = Photo.objects.all()
+    comment_form = CommentForm()
+    search_form = SearchForm()
+
+    if request.method == "POST":
+        search_form = SearchForm(request.POST)
+        if search_form.is_valid():
+            all_photos = all_photos.filter(tagged_pets__name__icontains=search_form.cleaned_data['pet_name'])
+
     context = {
-        'all_photos': all_photos
+        'all_photos': all_photos,
+        'comment_form': comment_form,
+        'search_form': search_form,
     }
+
     return render(request, 'common/home-page.html', context)
+
+
+def add_comment(request, photo_id):
+    if request.method == "POST":
+        photo = Photo.objects.get(id=photo_id)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.to_photo = photo
+            comment.save()
+
+        return redirect(request.META['HTTP_REFERER'] + f'#{photo_id}')
 
 
 def like_functionality(request, photo_id):
